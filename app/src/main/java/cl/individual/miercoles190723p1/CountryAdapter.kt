2fb285905e75e46169f944d4d727edcc77c5e21c
Emilia <correoalcantaraemilia@gmail.com>
2023-07-19 22:21:23 -0400

    Refactorización/reformat.

--- app/src/main/java/cl/individual/miercoles190723p1/CountryAdapter.kt
+++ app/src/main/java/cl/individual/miercoles190723p1/CountryAdapter.kt
@@ -1,10 +1,9 @@
 package cl.individual.miercoles190723p1
 
-import android.telecom.Call
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import cl.individual.miercoles190723p1.databinding.ItemBinding
 import coil.load
@@ -13,9 +12,6 @@ class CountryAdapter : RecyclerView.Adapter<CountryAdapter.MyViewHolder>() {
     var paisesListado = mutableListOf<Pais>()
     var callback: PaisCallback? = null
 
-    fun setPaisCallback(c: PaisCallback) {
-        this.callback = c
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -29,7 +25,6 @@ class CountryAdapter : RecyclerView.Adapter<CountryAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val pais: Pais = paisesListado[position]
         holder.bind(pais)
-      //  holder.itemView.setOnClickListener()
 
     }
 
@@ -44,13 +39,10 @@ class CountryAdapter : RecyclerView.Adapter<CountryAdapter.MyViewHolder>() {
             binding.txtCountry.text = pais.nombre
             binding.imgBandera.load(pais.imgUrl)
 
-
             val texto = "Pais: ${pais.nombre}, Población: ${pais.poblacion}"
-
 
             binding.countryCard.setOnClickListener(View.OnClickListener {
                 callback?.mostrarPoblacion(texto)
-               // Toast.makeText(itemView.context, texto, Toast.LENGTH_LONG).show()
             })
         }
 
--- app/src/main/java/cl/individual/miercoles190723p1/MainActivity.kt
+++ app/src/main/java/cl/individual/miercoles190723p1/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import cl.individual.miercoles190723p1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), PaisCallback {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -16,11 +16,11 @@ class MainActivity : AppCompatActivity(), PaisCallback {
     private fun initRecycler() {
         val countryAdapter = CountryAdapter()
 
-
         countryAdapter.setData(PaisesLatam.paises)
+        countryAdapter.callback = this
 
         binding.recCountryList.adapter = countryAdapter
-        countryAdapter.setPaisCallback(this)
+
     }
 
     override fun mostrarPoblacion(s: String) {
